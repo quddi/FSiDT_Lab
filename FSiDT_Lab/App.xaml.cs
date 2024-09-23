2712df9945b589333bb389ca74ca3dd82e800a47
Nikita Andrushko <quddi.tech@gmail.com>
2024-09-23 22:39:44 +0300

    First upload

---
+++ FSiDT_Lab/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FSiDT_Lab
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

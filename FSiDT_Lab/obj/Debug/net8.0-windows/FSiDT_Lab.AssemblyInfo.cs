59ffa22f3fb6a8d845b06e866698df49edb011c1
Nikita Andrushko <quddi.tech@gmail.com>
2024-09-23 22:53:18 +0300

    Windows folder

--- FSiDT_Lab/obj/Debug/net8.0-windows/FSiDT_Lab.AssemblyInfo.cs
+++ FSiDT_Lab/obj/Debug/net8.0-windows/FSiDT_Lab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FSiDT_Lab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2712df9945b589333bb389ca74ca3dd82e800a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("FSiDT_Lab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FSiDT_Lab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

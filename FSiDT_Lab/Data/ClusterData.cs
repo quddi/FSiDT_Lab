7d9c6d9c03638448d5f220e0dd9c44448e4dcb5e
Nikita Andrushko <quddi.tech@gmail.com>
2024-10-09 21:00:32 +0300

    Reorganized clusters data

---
+++ FSiDT_Lab/Data/ClusterData.cs
@@ -0,0 +1,11 @@
+﻿namespace FSiDT_Lab
+{
+    public class ClusterData
+    {
+        public int Index { get; set; }
+
+        public ScottPlot.Color Color { get; set; }
+
+        public Coordinates Coordinates { get; set; }
+    }
+}
